311429d1bd64ae40da22b01e0136777ea1a03d41
nhooven <nathan.d.hooven@gmail.com>
2026-09-15 09:35:44 -0700

    Added other unknown taxa; fixed the one Saxifragaceae entry

--- scripts/03 - Finalize plant taxa.R
+++ scripts/03 - Finalize plant taxa.R
@@ -4,7 +4,7 @@
 # EMAIL: nathan.d.hooven@gmail.com
 # BEGAN: 08 Sep 2026
 # COMPLETED: 08 Sep 2026
-# LAST MODIFIED: 14 Sep 2026
+# LAST MODIFIED: 15 Sep 2026
 # R VERSION: 4.5.2
 
 # ______________________________________________________________________________
@@ -14,11 +14,15 @@
 # after subsequent BLASTs, we should group all relevant identified ASVs into one
 # table, so we can attribute them to sample reads in the next script
 
+# we want to keep unidentified sequences in here, but ensure that bait/contaminants are
+# kicked out
+
 # ______________________________________________________________________________
 # 1. Load packages ----
 # ______________________________________________________________________________
 
 library(tidyverse)
+library(mefa4)
 
 # ______________________________________________________________________________
 # 2. Read in data ----
@@ -28,6 +32,9 @@ asvs.1 <- read.csv("data_cleaned/asvs_identified.csv")
 asvs.2 <- read.csv("data_cleaned/asvs_identified2.csv")
 asvs.3 <- read.csv("data_cleaned/asvs_identified3.csv")
 asvs.4 <- read.csv("data_cleaned/asvs_identified4.csv")
+
+asvs.5 <- read.csv("data_cleaned/asvs_unidentified.csv")
+asvs.6 <- read.csv("data_cleaned/asvs_unidentified2.csv")
 
 # ______________________________________________________________________________
 # 3. Clean and bind together ----
@@ -58,8 +65,39 @@ asvs.4 <- asvs.4 |> dplyr::select(ASV.unq, batchID, ASV,
                                   n_reads, cover, ident,
                                   taxa, family, genus, species)
 
+# bind together
+asvs.identified <- bind_rows(asvs.1, asvs.2, asvs.3, asvs.4)
+
+# unidentified taxa
+asvs.unidentified <- bind_rows(
+  
+  asvs.5 |> dplyr::select(ASV.unq, batchID, ASV,
+                          n_reads, cover, ident,
+                          taxon_inferred, family, genus, species) |>
+  
+    # keep only ones that aren't identified
+    filter(ASV.unq %notin% asvs.identified$ASV.unq),
+  
+  asvs.6 |> dplyr::select(ASV.unq, batchID, ASV,
+                          n_reads, cover, ident,
+                          taxa, family, genus, species) |>
+    
+    # keep only ones that aren't identified
+    filter(ASV.unq %notin% asvs.identified$ASV.unq)
+  
+) |>
+  
+  # add family
+  mutate(
+    
+    family = ifelse(family == "" | is.na(family) == T,
+                    "unknown",
+                    family)
+    
+  )
+
 # bind in
-asvs.all <- bind_rows(asvs.1, asvs.2, asvs.3, asvs.4)
+asvs.all <- bind_rows(asvs.identified, asvs.unidentified)
 
 # remove contaminant reads
 asvs.all <- asvs.all[-which(asvs.all$genus == "Glycine"), ]
@@ -122,6 +160,9 @@ asvs.all.1 <- asvs.all |>
       # just a family (no sub-group)
       genus == "" & species == "" & sub.group == "" ~ family,
       
+      # unknown family (there's NAs in here)
+      is.na(genus) == T ~ family,
+      
       # just a genus (no sub-group)
       genus != "" & species == "" & sub.group == "" ~ genus,
       
@@ -153,6 +194,7 @@ asvs.all.1 <- asvs.all |>
                    "Abies",
                    "Juniperus",
                    "Pseudotsuga") ~ "conifer",
+      
       genus %in% c("Shepherdia",
                    "Alnus",
                    "Rubus",
@@ -227,11 +269,12 @@ asvs.all.1 <- asvs.all |>
                    "Cerastium",
                    "Cornus",
                    "Galium",
-                   "Stellaria") ~ "forb",
+                   "Stellaria",
+                   "Lithophragma / Heuchera") ~ "forb",
       
       family %in% c("Poaceae", "Cyperaceae") ~ "graminoid",
       
-      genus %in% c("") ~ "unknown"
+      genus %in% c("") | is.na(genus) == T ~ "unknown"
       
     )
     
@@ -243,6 +286,15 @@ asvs.all.1 <- asvs.all |>
     cat2 = ifelse(species == "contorta",
                   "lodgepole pine",
                   cat1)
+    
+  ) |>
+  
+  # impute "unknown" for NA
+  mutate(
+    
+    cat2 = ifelse(is.na(cat2) == T,
+                  "unknown",
+                  cat2)
     
   )
 
